c4ca30008c16506d7bbc75d0b983cd9116a8063a
niko0022 <nikolozishotniashvili@gmail.com>
2024-07-28 12:06:55 +0400

    call the function to create the divs 16 by 16

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
-const container = document.getElementById("container")
+const container = document.getElementById("container");
 
 function createDivs(size) {
     for (let i = 0; i < size * size; i++) {
-        const gridItem = document.createElement("div")
-        gridItem.classList.add("grid-item")
-        container.appendChild(gridItem)
+        const gridItem = document.createElement("div");
+        gridItem.classList.add("grid-item");
+        container.appendChild(gridItem);
     }
 }
+
+createDivs(16)
