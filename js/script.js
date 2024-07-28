b8360166a0dd697a037d89129109c95dde839f2b
niko0022 <nikolozishotniashvili@gmail.com>
2024-07-28 13:19:07 +0400

    Add mouseover event listiner and create random rgb color function to generate random collor when you hover over a div block

--- js/script.js
+++ js/script.js
@@ -4,8 +4,22 @@ function createDivs(size) {
     for (let i = 0; i < size * size; i++) {
         const gridItem = document.createElement("div");
         gridItem.classList.add("grid-item");
+
+        gridItem.addEventListener("mouseover", () => {
+            gridItem.classList.add('hovered');
+            const randomColor = getRandomColor();
+            gridItem.style.backgroundColor = randomColor;
+
+        });
         container.appendChild(gridItem);
-    }
+    } 
 }
 
-createDivs(16)
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+createDivs(16);
