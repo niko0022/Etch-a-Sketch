6ad4c2fd7381605dbfe53cf172f6df0742a85c4b
niko0022 <nikolozishotniashvili@gmail.com>
2024-07-28 11:36:07 +0400

    Add js directory and add javascript file also add createDivs function

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const container = document.getElementById("container")
+
+function createDivs(size) {
+    for (let i = 0; i < size * size; i++) {
+        const gridItem = document.createElement("div")
+        gridItem.classList.add("grid-item")
+        container.appendChild(gridItem)
+    }
+}
